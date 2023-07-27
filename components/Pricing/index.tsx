c926010d679185ebceb9c32d65e8ee40c75e0286
César Flores <florescesar80@gmail.com>
2023-07-27 17:21:51 -0300

    change images

--- components/Pricing/index.tsx
+++ components/Pricing/index.tsx
@@ -25,7 +25,7 @@ const Pricing = () => {
                   <ul className="flex-1 mb-6 dark:text-white">
                     {
                       pricing.includes.map(filterIncludes => (
-                        <li className="flex mb-2 space-x-2">
+                        <li className="flex mb-2 space-x-2" key="">
                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-white">
                             <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                           </svg>
